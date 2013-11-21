43d3617de732ce881381ef285dce98cbde140eac
Marcelo Reis <marcello@marcelloreis.com>
2013-11-21 04:37:16 -0200

    First Commit

---
+++ app/Config/bootstrap_app.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Textos padroes da aplicação
+ */	
+define('TITLE_HEADER', 'NZ Eventos');
+define('TITLE_APP', 'NZ Eventos');
+define('VERSION_APP', '1.0');
+define('COPYRIGHT', 'Nasza Produtora');
+define('COPYRIGHT_LINK', 'http://nasza.com.br');
+
+
+/**
+ * IDs padroes do projeto
+ */
+//Tipos de patrocinadores
+define('SPONSOR_DIAMOND', 1);
+define('SPONSOR_GOLD', 2);
+define('SPONSOR_SILVER', 3);
+//Sexo
+define('FEMALE', 1);
+define('MALE', 2);
+//Tamanho de camisetas
+define('BABY_PEQUENA', 1);
+define('BABY_MEDIA', 2);
+define('BABY_GRANDE', 3);
+define('PEQUENA', 4);
+define('MEDIA', 5);
+define('GRANDE', 6);
+define('EXTRA_GRANDE', 7);
+//Grau de Escolaridade
+define('SUPERIOR_COMPLETO', 1);
+define('SUPERIOR_INCOMPLETO', 2);
+define('MEDIO_COMPLETO', 3);
+define('MEDIO_INCOMPLETO', 4);
+//Tipos de pagamento
+define('PAGSEGURO', 1);
+define('DINHEIRO', 2);
